947a21e509f14d413b6e000b849919e2588cb0d3
Connor Roane <connorgroane@utexas.edu>
2023-02-17 09:18:55 -0800

    revert to getstaticprops for tables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { GlobalStyles, theme } from '@/styles/global'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { PoolTableContextWrapper } from '@/state/PoolTablesProvider';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -12,5 +13,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <Component {...pageProps} />
       </PoolTableContextWrapper>
     </ThemeProvider>
+    <Analytics />
   </>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,16 @@
 import BottomBar from "@/components/BottomBar";
 import Map from "@/components/maps";
 import Page from "@/components/page";
-import { useMongo } from "@/hooks/useMongo";
 import { PoolTable } from "@/types";
-import { Suspense, useCallback, useEffect, useState } from "react";
-// import { getPoolTables } from "./api/tables";
+import { Suspense, useState } from "react";
+import { getPoolTables } from "./api/tables";
 import Loading from "./loading";
 
 interface HomeProps {
   tables: PoolTable[]
 }
-const Home: React.FC<HomeProps> = ({  }) => {
+const Home: React.FC<HomeProps> = ({ tables }) => {
   const [selectedTable, setSelectedTable] = useState<PoolTable>({ } as PoolTable);
-  const [tables, setTables] = useState<PoolTable[]>([])
-  const app = useMongo();
-
-  const getTables = useCallback(() => {
-    if (app && app.currentUser) {
-      app.currentUser.mongoClient("mongodb-atlas").db('pool-tables').collection('pool-taables').find()
-        .then((response) => {
-          setTables(response);
-        })
-    }
-  }, [setTables, app])
-  console.log('husky');
-  useEffect(() => {
-    getTables();
-  },[getTables])
-
   return (
     <Suspense fallback={<Loading />}>
       <Page>
@@ -38,5 +21,23 @@ const Home: React.FC<HomeProps> = ({  }) => {
   )
 }
 
+
+
+/**
+ * 
+ * @returns this is for SSR pool table findings\
+ * although it may be  better to just do it all client side for realtime updates
+ */
+export const getStaticProps = async () => {
+  const data = await getPoolTables();
+  return {
+    props: {
+      tables: JSON.parse(JSON.stringify(data))
+    }
+  }
+}
+// const getStaticPaths = () => {
+  
+// }
 
 export default Home;
