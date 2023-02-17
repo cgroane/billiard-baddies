11ea6fd89b218aa2987cd65f301ce6e20109a47c
Connor Roane <connorgroane@utexas.edu>
2023-02-16 17:12:45 -0800

    fix radio button, add some error handling/ response

--- src/components/NewTableForm/index.tsx
+++ src/components/NewTableForm/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useRef, ChangeEvent, useMemo, FormEvent, useCallback }
 import styled from 'styled-components'
 import { useRouter } from 'next/router';
 import { useGoogleAutocomplete } from '@/utils/handleGoogleScriptLoad';
+import { Span } from '../shared';
 
 interface NewTableFormProps {}
 interface NewTableData extends PoolTableAutoFillData {
@@ -12,6 +13,7 @@ interface NewTableData extends PoolTableAutoFillData {
 const NewTableForm: React.FC<NewTableFormProps> = ({}: NewTableFormProps) => {
   const router = useRouter();
   const inputRef = useRef<HTMLInputElement>(null);
+  const [error, setError] = useState('')
   const {poolTableData, handleChangeManual} = useGoogleAutocomplete(inputRef, {} as NewTableData);
   const [manualFormData, setManualFormData] = useState<{ cost: string; rate: Rates }>({
     cost: '',
@@ -39,12 +41,13 @@ const NewTableForm: React.FC<NewTableFormProps> = ({}: NewTableFormProps) => {
       },
       body: JSONData
     }
-    const response = await fetch(endpoint, options)
-
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-    const result = await response.json()
+    try {
+      const response = await fetch(endpoint, options)
+      const result = await response.json()
     router.push('/');
+    } catch (error) {
+      setError('Could not add this pool table');
+    }
   }
 
   const handleCostAndRate = (e: ChangeEvent<HTMLInputElement>) => {
@@ -53,7 +56,8 @@ const NewTableForm: React.FC<NewTableFormProps> = ({}: NewTableFormProps) => {
       ...manualFormData,
       [name]: value
     })
-  }
+  };
+
   const validateAddress = useCallback(() => {
     const addressFields = Object.keys(poolTableData.address);
     let i;
@@ -61,6 +65,7 @@ const NewTableForm: React.FC<NewTableFormProps> = ({}: NewTableFormProps) => {
     for (i = 0; i < addressFields.length - 1; i ++) {
       if (addressFields[i] !== 'address2' && !poolTableData.address[addressFields[i] as keyof Address].length) {
         isEmpty = true;
+        setError('Fill out all required fields')
         break;
       } else {
         isEmpty = false
@@ -81,25 +86,29 @@ const NewTableForm: React.FC<NewTableFormProps> = ({}: NewTableFormProps) => {
       <RadioGroup>
         <label>
           Hourly
-          <Radio onChange={handleCostAndRate} id="hourly" type="radio" value={Rates.hourly}/>
+          <Radio onChange={handleCostAndRate} name="rate" id="hourly" type="radio" value={Rates.hourly} checked={manualFormData.rate === Rates.hourly}/>
         </label>
         <label>
           Per game
-          <Radio onChange={handleCostAndRate} id="pergame" type="radio" value={Rates.perGame}/>
+          <Radio onChange={handleCostAndRate} name="rate" id="pergame" type="radio" value={Rates.perGame} checked={manualFormData.rate === Rates.perGame}/>
         </label>
       </RadioGroup>
       <SubmitButton type='submit' disabled={disable} >Add To Map</SubmitButton>
+      {error && <Span fontSize='medium' >{error}</Span>}
   </StyledForm>
   )
 }
  
 const StyledInput = styled.input`
-  border: 0.1563rem solid ${props => props.theme.colors.darkMoss};
+  border: 0.1563rem solid ${props => props.theme.colors.white};
   border-right: none;
   border-left: none;
   margin: 1rem auto;
   width: 70%;
   height: 2.5rem;
+  background: none;
+  color: white;
+
   :focus {
     outline: none;
   }
@@ -118,13 +127,15 @@ const SubmitButton = styled.button`
   width: 10rem;
   padding: 0.5rem;
   font-size: 1rem;
-  background: ${props => props.theme.colors.white};
-  color: ${props => props.theme.colors.darkMoss};
+  background: ${props => props.theme.colors.black};
+  color: ${props => props.theme.colors.white};
   border: none;
   border-radius: 8px;
-  border: 0.125rem solid ${props => props.theme.colors.darkMoss};
+  border: 0.125rem solid ${props => props.theme.colors.white};
   &:disabled {
-    background: grey;
+    background: #EBEBE4;
+    
+    color: white
   }
 `
 const RadioGroup = styled.div`
